36fe7dc70f3b6ac8af65cef54026080db7dd0f39
JJERANEK <jj_marie_jeranek@yahoo.com>
2023-01-27 22:30:59 -0600

    WebPage 95% Complete

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,7 +2,7 @@
 
 // Use this link to get the GeoJSON data.
 let geoData = "data/counties.fips.geojson";
-let usda_data = "data/usda_data.json";
+let usda_data = "https://project-3-team-8.onrender.com/api/v1.0/usda_data";
 let filenames = [geoData, usda_data];
 
 
@@ -12,38 +12,23 @@ function buildCharts(FIPS) {
 
     // Read in the JSON data
     d3.json(usda_data).then((data => {
-        // let county = data[0]
-        // Define variables
-        // let samples = data.samples
-        // let metadata = data.metadata
+
         let filteredData = data.filter(item => item.FIPS == FIPS)[0]
+
+        let barchartdata = [filteredData.Fast_Food_Restaurants_Per_1000_of_Population, filteredData.Grocery_Stores_Per_1000_of_Population, filteredData.Farmers_Markets_Per_1000_of_Population]
   
-        // Filter by patient ID
-        // let filteredSample = samples.filter(bacteriaInfo => bacteriaInfo.id == patientID)[0]
-  
-        // Create variables for chart
-        // Grab sample_values for the bar chart
-        let barchartdata = [filteredData.Fast_Food_Restaurants, filteredData.Grocery_Stores, filteredData.Farmers_Markets]
-  
-        // Use otu_ids as the labels for bar chart
         let barchartlabel = ["Fast Food", "Grocery Stores", "Farmer's Markets"]
   
-        // // use otu_labels as the hovertext for bar chart
-        // let otu_labels = filteredSample.otu_labels
   
         // BAR CHART
         // Create the trace
         let bar_data = [{
-            // Use otu_ids for the x values
             y: barchartdata,
-            // Use sample_values for the y values
             x: barchartlabel,
-            // // Use otu_labels for the text values
-            // text: otu_labels.slice(0, 10).reverse(),
             type: 'bar',
             // orientation: 'h',
             marker: {
-                color: 'rgb(28, 145, 88)'
+                color: 'rgb(33, 40, 166)'
             },
         }]
   
@@ -52,7 +37,7 @@ function buildCharts(FIPS) {
   
         // Define plot layout
         let bar_layout = {
-            title: `${filteredData.County}, ${filteredData.State}`,
+            title: `Establishments Per 1,000 Residents<br>${filteredData.County}, ${filteredData.State}`,
             xaxis: { title: "Number of Locations", "autorange": true},
             paper_bgcolor:"black",
             font: {color:'#ffffff'}
@@ -64,23 +49,24 @@ function buildCharts(FIPS) {
   
         // GAUGE CHART
         // Create variable for washing frequency
-        let obesity = filteredData['Obesity_(%)']
+        let obesity = filteredData['Obesity']
   
         // Create the trace
         let gauge_data = [
             {
                 domain: { x: [0, 1], y: [0, 1] },
                 value: obesity,
-                title: { text: "Obesity Percentage for the State" },
+                number: {'suffix': "%"},
+                title: { text: "Obesity for the State" },
                 type: "indicator",
                 mode: "gauge+number",
                 gauge: {
-                    bar: {color: 'cornflowerblue'},
+                    bar: {color: 'rgb(33, 40, 166)'},
                     axis: { range: [null, 100] },
                     steps: [
-                        { range: [0, 33], color: 'rgb(193, 245, 220)' },
-                        { range: [33, 66], color: 'rgb(86, 186, 138)' },
-                        { range: [66, 100], color: 'rgb(17, 115, 68)' },
+                        { range: [0, 33], color: 'rgb(26, 107, 28)' },
+                        { range: [33, 66], color: 'rgb(208, 217, 43)' },
+                        { range: [66, 100], color: 'rgb(107, 4, 4)' },
                     ],
                 }
             }
@@ -97,7 +83,7 @@ function buildCharts(FIPS) {
   };
   
   
-  // FUNCTION #2 of 4 - Populate the Demographic Info box
+  // FUNCTION #2 of 4 - Populate the County Data box
   function populateDemoInfo(FIPS) {
   
     let demographicInfoBox = d3.select("#sample-metadata");
@@ -117,7 +103,7 @@ function buildCharts(FIPS) {
     })
   }
   
-  // FUNCTION #3 of 4 - Changes what is shown dependent on ID selection
+  // FUNCTION #3 of 4 - Changes what is shown dependent on click selection
   function optionChanged(FIPS) {
     console.log(FIPS);
     buildCharts(FIPS);
@@ -131,9 +117,6 @@ function buildCharts(FIPS) {
         let FIPS_Nums = [];
         for (let i = 0; i < data.length; i++) {
             FIPS_Nums.push(data[i].FIPS)
-        // }
-        // console.log(FIPS);
-        // FIPS_Num.forEach(FIPS => {
             dropdown.append("option").text(data[i].FIPS).property("value", data[i].FIPS)
         }
         buildCharts(FIPS_Nums[0]);
@@ -143,6 +126,8 @@ function buildCharts(FIPS) {
   
   initDashboard();
 
+
+  // Granim footer
   let granimInstance = new Granim({
     element: '#canvas-basic',
     direction: 'left-right',
--- static/js/map.js
+++ static/js/map.js
@@ -1,44 +1,245 @@
 let myMap = L.map("map", {
-  center: [35.96044, -90.30695],
+  center: [37.09, -95.71],
   zoom: 5
 });
 
 // Adding the tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+let Base = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-let geoData2 = "data/georef-united-states-of-america-county-millesime.geojson"
-let usdaData2 = "data/usda_data.json"
-let geojson2;
+// Fetch GeoJSON and data to join to it
+$.when(
+  $.getJSON("data/counties.fips.geojson"),
+  $.getJSON("https://project-3-team-8.onrender.com/api/v1.0/usda_data")
+  ).done(function (responseGeojson, responseData) {
+    var data = responseData[0]
+    var DATAHOLDER = responseGeojson[0]
+    // Create hash table for easy reference
+    var dataHash = data.reduce(function (hash, item) {
+      if (parseInt(item.FIPS)) {
+        hash[parseInt(item.FIPS)] = item
+      }
+      return hash
+    }, {})
+    // Add value from hash table to geojson properties
+DATAHOLDER.features.forEach(function (item) {
+      item.properties = dataHash[parseInt(item.properties.FIPS)] || null
+    })
+//console.log(geojson.features.filter(item => item.properties.FIPS==1001))
+    //item.properties.USDA.FIPS
+    console.log(DATAHOLDER)
+    //console.log(item.properties.USDA.Grocery_Stores)
+gs=[]
+for (var i = 0; i < DATAHOLDER.features.length; i++){
+  try{
+gs.push(DATAHOLDER.features[i].properties.USDA['Grocery_Stores'])
+  }
+catch(TypeError){
+gs.push(0)}
+}
+DATAHOLDER.features=DATAHOLDER.features.filter(feature=> feature.properties)
+//geojson.features[0]
+console.log(DATAHOLDER)
+//console.log(gs)
+console.log(DATAHOLDER.features[1].properties)
+    let obesitylayer = L.choropleth(DATAHOLDER, {
+      valueProperty:function(feature){
+        console.log(feature)
+        return feature.properties.Obesity}
+     ,
+      scale: ["#F7FCFD", "#4D004B"],
+      steps: 6,
+      mode: "q",
+      style: {
+        // Border color
+        color: "#000000",
+        weight: 1,
+        fillOpacity: 0.8
+      },
+          // This is called on each feature.
+    onEachFeature: function(feature, layer) {
+      console.log(feature);
+      let FIPS_Num = feature.properties.FIPS;
+      let CountyData = feature.properties.County;
+      let StateData = feature.properties.State;
+      let PopData = feature.properties.Population_Estimate_2018
+      let FFRData = feature.properties.Fast_Food_Restaurants
+      let GroceryData = feature.properties.Grocery_Stores
+      let FMData = feature.properties.Farmers_Markets
+      // Set the mouse events to change the map styling.
+      layer.on({
 
-// // Get the data with d3.
-// d3.json(geoData2).then(function(data) {
-//   // console.log(data)
-//   // Create a new choropleth layer.
-//   geojson2 = L.choropleth(data, {
+        // When a feature (neighborhood) is clicked, it enlarges to fit the screen.
+        click: function(event) {
+          myMap.fitBounds(event.target.getBounds());
+          optionChanged(FIPS_Num);
+        }
+        
+      });
+      // Giving each feature a popup with information that's relevant to it
+      layer.bindPopup("<h1>" + CountyData + ", " + StateData + "</h1><hr><h4> Population: " + PopData + "</h4><hr><h4> Fast Food Restaurants: " + 
+        FFRData + "</h4><hr><h4> Grocery Stores: " + GroceryData + "</h4><hr><h4> Farmers Markets: " + FMData + "</h4>");
 
-//     // Define which property in the features to using a function for a separate data source.
-//     valueProperty: function(feature) {
-//         return usdaData2.filter(item => item.FIPS = feature.properties.FIPS)[0].Population_Estimate_2018
-//     } ,
-    
-    
-//     // // Define which property in the features to use.
-//     // valueProperty: "CENSUSAREA",
-//     scale: ["#ffffb2", "#b10026"],
-//     steps: 10,
-//     mode: "q",
-//     style: {
-//       // Border color
-//       color: "#000000",
-//       weight: 1,
-//       fillOpacity: 0.8
-//     },
+    }
 
-//     // Binding a popup to each layer
-//     onEachFeature: function(feature, layer) {
-//       layer.bindPopup(`<strong> ${feature.properties.COUNTY}, ${feature.properties.STATE}</strong>`);
-//     }
-//   }).addTo(myMap);
-// });
+    }).addTo(myMap)
+
+    // FAST FOOD LAYER
+    let fast_food_layer = L.choropleth(DATAHOLDER, {
+      valueProperty:function(feature){
+        console.log(feature)
+        return feature.properties.Fast_Food_Restaurants}
+     ,
+      scale: ["#F7FCFD", "#4D004B"],
+      steps: 6,
+      mode: "q",
+      style: {
+        // Border color
+        color: "#000000",
+        weight: 1,
+        fillOpacity: 0.8
+      },
+          // This is called on each feature.
+    onEachFeature: function(feature, layer) {
+      console.log(feature);
+      let FIPS_Num = feature.properties.FIPS;
+      let CountyData = feature.properties.County;
+      let StateData = feature.properties.State;
+      let PopData = feature.properties.Population_Estimate_2018
+      let FFRData = feature.properties.Fast_Food_Restaurants
+      let GroceryData = feature.properties.Grocery_Stores
+      let FMData = feature.properties.Farmers_Markets
+      // Set the mouse events to change the map styling.
+      layer.on({
+        
+        // When a feature (neighborhood) is clicked, it enlarges to fit the screen.
+        click: function(event) {
+          myMap.fitBounds(event.target.getBounds());
+          optionChanged(FIPS_Num);
+        }
+      });
+      // Giving each feature a popup with information that's relevant to it
+      layer.bindPopup("<h1>" + CountyData + ", " + StateData + "</h1><hr><h4> Population: " + PopData + "</h4><hr><h4> Fast Food Restaurants: " + 
+        FFRData + "</h4><hr><h4> Grocery Stores: " + GroceryData + "</h4><hr><h4> Farmers Markets: " + FMData + "</h4>");
+
+    }
+
+    });
+
+    // GROCERY STORE LAYER
+    let grocery_layer = L.choropleth(DATAHOLDER, {
+      valueProperty:function(feature){
+        console.log(feature)
+        return feature.properties.Grocery_Stores}
+     ,
+      scale: ["#F7FCFD", "#4D004B"],
+      steps: 6,
+      mode: "q",
+      style: {
+        // Border color
+        color: "#000000",
+        weight: 1,
+        fillOpacity: 0.8
+      },
+          // This is called on each feature.
+    onEachFeature: function(feature, layer) {
+      console.log(feature);
+      let FIPS_Num = feature.properties.FIPS;
+      let CountyData = feature.properties.County;
+      let StateData = feature.properties.State;
+      let PopData = feature.properties.Population_Estimate_2018
+      let FFRData = feature.properties.Fast_Food_Restaurants
+      let GroceryData = feature.properties.Grocery_Stores
+      let FMData = feature.properties.Farmers_Markets
+      // Set the mouse events to change the map styling.
+      layer.on({
+        
+        // When a feature (neighborhood) is clicked, it enlarges to fit the screen.
+        click: function(event) {
+          myMap.fitBounds(event.target.getBounds());
+          optionChanged(FIPS_Num);
+        }
+      });
+      // Giving each feature a popup with information that's relevant to it
+      layer.bindPopup("<h1>" + CountyData + ", " + StateData + "</h1><hr><h4> Population: " + PopData + "</h4><hr><h4> Fast Food Restaurants: " + 
+        FFRData + "</h4><hr><h4> Grocery Stores: " + GroceryData + "</h4><hr><h4> Farmers Markets: " + FMData + "</h4>");
+
+    }
+
+    });
+
+    // FARMERS MARKETS LAYER
+    let farmers_markets_layer = L.choropleth(DATAHOLDER, {
+      valueProperty:function(feature){
+        console.log(feature)
+        return feature.properties.Farmers_Markets}
+     ,
+      scale: ["#F7FCFD", "#4D004B"],
+      steps: 6,
+      mode: "q",
+      style: {
+        // Border color
+        color: "#000000",
+        weight: 1,
+        fillOpacity: 0.8
+      },
+          // This is called on each feature.
+    onEachFeature: function(feature, layer) {
+      console.log(feature);
+      let FIPS_Num = feature.properties.FIPS;
+      let CountyData = feature.properties.County;
+      let StateData = feature.properties.State;
+      let PopData = feature.properties.Population_Estimate_2018
+      let FFRData = feature.properties.Fast_Food_Restaurants
+      let GroceryData = feature.properties.Grocery_Stores
+      let FMData = feature.properties.Farmers_Markets
+      // Set the mouse events to change the map styling.
+      layer.on({
+        
+        // When a feature (neighborhood) is clicked, it enlarges to fit the screen.
+        click: function(event) {
+          myMap.fitBounds(event.target.getBounds());
+          optionChanged(FIPS_Num);
+        }
+      });
+      // Giving each feature a popup with information that's relevant to it
+      layer.bindPopup("<h1>" + CountyData + ", " + StateData + "</h1><hr><h4> Population: " + PopData + "</h4><hr><h4> Fast Food Restaurants: " + 
+        FFRData + "</h4><hr><h4> Grocery Stores: " + GroceryData + "</h4><hr><h4> Farmers Markets: " + FMData + "</h4>");
+
+    }
+
+    });
+
+  let baseMaps = {
+    "Obesity": obesitylayer,
+    "Fast Food": fast_food_layer,
+    "Grocery Stores": grocery_layer,
+    "Farmers Markets": farmers_markets_layer
+  };
+
+  L.control.layers(baseMaps).addTo(myMap)
+
+
+  /*Legend*/
+  let legend = L.control({ position: 'bottomright' });
+  legend.onAdd = function (myMap) {
+    let div = L.DomUtil.create('div', 'info legend')
+    let colors = obesitylayer.options.colors
+    let labels = []
+
+    /* Add min & max*/
+    div.innerHTML = '<div class="labels"><div class="min">Low</div> \
+      <div class="max">High</div></div>'
+
+    for (i = 1; i < colors.length; i++) {
+      labels.push('<li style="background-color: ' + colors[i] + '"></li>')
+    }
+
+    div.innerHTML += '<ul style="list-style-type:none;display:flex">' + labels.join('') + '</ul>'
+    return div
+  }
+
+  legend.addTo(myMap);
+
+});
